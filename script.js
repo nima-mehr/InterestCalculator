96cc66c99551f9529fe33efdbd469343876b372e
Nima <N.mehr27@gmail.com>
2022-08-07 22:27:55 +0300

    Updated
    bugs fixed.

--- script.js
+++ script.js
@@ -1,16 +1,17 @@
+function compute()
+{
+    var p = document.getElementById("principal").value;
     var principal = document.getElementById("principal").value;
+    if (principal <= 0)
+     alert("Please enter a positive number.");
     var rate = document.getElementById("rate").value;
     var years = document.getElementById("years").value;
-    var interest = principal*years*rate/100;
+    var interest = principal * years * rate /100;
     var year = new Date().getFullYear()+parseInt(years);
-function updateRate() 
+    document.getElementById("result").innerHTML="if you deposit: "+principal + ",\<br\>at an interest rate of "+rate + ",\<br\>You will receive an amount of: "+interest+",\<br\>in the year: "+year;
+}
+    function updateRate()
 {
     var rateval = document.getElementById("rate").value;
     document.getElementById("rate_val").innerText=rateval;
 }
-function compute()
-{
-    p = document.getElementById("principal").value;
-    
-}
-    document.getElementById("result").innerHTML="If you deposit "+principal+",\<br\>at an interest rate of "+rate+"%\<br\>You will receive an amount of "+amount+",\<br\>in the year "+year+"\<br\>"
